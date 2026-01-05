00917b15e8cb1c1e8e6c4d7b584aa75634128fd5
Khalidhusain786 <75180014+Khalidhusain786@users.noreply.github.com>
2026-01-05 11:46:42 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
-#Author: Khalid Husain
-#Instagram: khalidhusain786
-trap 'echo exiting cleanly...; exit 1;' SIGINT SIGTSTP
+# Install script for Instashell
+# Coded by: Khalid Husain
 
-checkroot() {
+echo -e "\e[1;92m[*] Updating packages...\e[0m"
+sudo apt update
 
-if [[ "$(id -u)" -ne 0 ]]; then
-   printf "\e[1;77mPlease, run this program as root!\n\e[0m"
-   exit 1
-fi
+echo -e "\e[1;92m[*] Installing Tor, Curl, and OpenSSL...\e[0m"
+sudo apt install -y tor curl openssl
 
-}
+echo -e "\e[1;92m[*] Setting permissions for instashell.sh...\e[0m"
+chmod +x instashell.sh
 
-checkroot
+echo -e "\e[1;92m[*] Starting Tor service...\e[0m"
+sudo service tor start
 
-(trap '' SIGINT SIGTSTP && command -v tor > /dev/null 2>&1 || { printf >&2  "\e[1;92mInstalling TOR, please wait...\n\e[0m"; apt-get update > /dev/null && apt-get -y install tor > /dev/null || printf "\e[1;91mTor Not installed.\n\e[0m"; }) & wait $!
-
-(trap '' SIGINT SIGTSTP && command -v openssl > /dev/null 2>&1 || { printf >&2  "\e[1;92mInstalling openssl, please wait...\n\e[0m"; apt-get update > /dev/null && apt-get -y install openssl > /dev/null || printf "\e[1;91mOpenssl Not installed.\n\e[0m"; }) & wait $! 
-
-(trap '' SIGINT SIGTSTP && command -v curl > /dev/null 2>&1 || { printf >&2  "\e[1;92mInstalling cURL, please wait...\n\e[0m"; apt-get update > /dev/null && apt-get -y install curl > /dev/null || printf "\e[1;91mCurl Not installed.\n\e[0m"; }) & wait $!
-
-printf "\e[1;92mAll Requires are installed!\n\e[0m"
+echo -e "\e[1;92m[+] Installation Complete!\e[0m"
+echo -e "\e[1;93m[!] Run the tool using: sudo ./instashell.sh\e[0m"
