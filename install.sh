1cd3f6f6952bc4d0177379934aeadeaf52ffd6bb
Khalidhusain786 <75180014+Khalidhusain786@users.noreply.github.com>
2026-01-05 11:48:22 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,17 +2,17 @@
 # Install script for Instashell
 # Coded by: Khalid Husain
 
-echo -e "\e[1;92m[*] Updating packages...\e[0m"
+echo -e "\e[1;92m[*] Updating system packages...\e[0m"
 sudo apt update
 
-echo -e "\e[1;92m[*] Installing Tor, Curl, and OpenSSL...\e[0m"
+echo -e "\e[1;92m[*] Installing dependencies (Tor, Curl, OpenSSL)...\e[0m"
 sudo apt install -y tor curl openssl
 
-echo -e "\e[1;92m[*] Setting permissions for instashell.sh...\e[0m"
+echo -e "\e[1;92m[*] Setting execution permissions...\e[0m"
 chmod +x instashell.sh
 
 echo -e "\e[1;92m[*] Starting Tor service...\e[0m"
 sudo service tor start
 
 echo -e "\e[1;92m[+] Installation Complete!\e[0m"
-echo -e "\e[1;93m[!] Run the tool using: sudo ./instashell.sh\e[0m"
+echo -e "\e[1;93m[!] To start the attack, run: sudo ./instashell.sh\e[0m"
